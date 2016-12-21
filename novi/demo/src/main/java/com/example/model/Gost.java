226d1f390e4af6fba13856a878a7cdddc1793b2b
Laptop-234234\Korisnik <milka94ns@gmail.com>
2016-12-21 17:35:23 +0100

    inicijalni projekat

---
+++ novi/demo/src/main/java/com/example/model/Gost.java
@@ -0,0 +1,77 @@
+package com.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Gost {
+
+
+	 @Column(name="ime", unique=false, nullable=false)
+	 private String ime;
+	 @Column(name="prezime", unique=false, nullable=false)
+	 private String prezime;
+	 @Column(name="email", unique=false, nullable=false)
+	 private String email;
+	 @Column(name="korisnickoIme", unique=false, nullable=false)
+	 private String korisnickoIme;
+	 @Column(name="sifra", unique=false, nullable=false)
+	 private String sifra;
+	 //private ArrayList<Gost>prijatelji;
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.IDENTITY)
+	 @Column(name="id", unique=true, nullable=false)
+	 private Long id;
+	 
+	 public Gost(){
+	   
+	 }
+
+	 public String getIme() {
+	  return ime;
+	 }
+	 public void setIme(String ime) {
+	  this.ime = ime;
+	 }
+	 public String getPrezime() {
+	  return prezime;
+	 }
+	 public void setPrezime(String prezime) {
+	  this.prezime = prezime;
+	 }
+	 public String getEmail() {
+	  return email;
+	 }
+	 public void setEmail(String email) {
+	  this.email = email;
+	 }
+
+	 public String getSifra() {
+	  return sifra;
+	 }
+	 public void setSifra(String sifra) {
+	  this.sifra = sifra;
+	 }
+	/* public ArrayList<Gost> getPrijatelji() {
+	  return prijatelji;
+	 }
+	 public void setPrijatelji(ArrayList<Gost> prijatelji) {
+	  this.prijatelji = prijatelji;
+	 }*/
+
+	 public String getKorisnickoIme() {
+	  return korisnickoIme;
+	 }
+
+	 public void setKorisnickoIme(String korisnickoIme) {
+	  this.korisnickoIme = korisnickoIme;
+	 }
+	 
+	 
+	 
+	}
+
+
